6b36e5a307b9c2095f721135238db78452a120ae
Baekhyeon <baeks215@gmail.com>
2026-06-09 13:55:11 +0100

    test: ring

---
+++ haskell-demo/ring.hs
@@ -0,0 +1,23 @@
+import Diagrams.Backend.SVG.CmdLine
+import Diagrams.Prelude
+
+n :: Int
+n = 6
+ringRadius :: Double
+ringRadius = 50
+spreadRadius :: Double
+spreadRadius = 80
+
+makeDot :: Int -> Diagram B
+makeDot i =
+  circle ringRadius # lw 2 # translate (r2 (x, y))
+ where
+  angle = 2 * pi * fromIntegral i / fromIntegral n
+  x = spreadRadius * cos angle
+  y = spreadRadius * sin angle
+
+rings :: Diagram B
+rings = mconcat [makeDot i | i <- [0 .. n - 1]]
+
+main :: IO ()
+main = mainWith rings
